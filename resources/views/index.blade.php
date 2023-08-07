a134c654c70ce7f849dcce2b130f0abced316ebb
Ibrahim <Ibrahim@gmail.com>
2023-08-07 15:48:33 +0300

    firtst commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+    {{-- HERO --}}
+    <div  class="container" class="container" >
+        <img src="https://picsum.photos/id/279/960/620" alt="">
+            <div style="text-align:center;">
+                <h1>Welcome to my blog</h1>
+                <a href="/">Start Reading</a>
+            </div>
+
+    </div>
+
+    {{-- HWO TO AN expert --}}
+    <div  class="container">
+        <div>
+            <img  src="https://picsum.photos/id/239/444/444" alt="">
+        </div>
+            <div style="text-align: right;">
+            <h2 style="font: bold;">How to expert in 2023</h2>
+            <p style="font: bold;">
+                you can find list of programming languages her
+            </p>
+            <p>
+                Get the same image
+            </p>
+            <a href="">Read More</a>
+            </div>
+        </div>
+    </div>
+
+{{-- Blog Categories --}}
+<div>
+    <h2>
+        Blog Categories
+    </h2>
+     <div>
+         <span>
+             
+         </span>
+     </div>
+</div>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,42 @@
+{{-- <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" /> --}}
+<footer class="navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+        <div>
+
+            <h5 style="color:gray;">Pages</h5>
+            <ul ">
+                <li style="color:gray;>
+                    <a href="/">home</a>
+                </li >
+                <li style="color:gray;>
+                    <a href="/blog">Blog</a>
+                </li>
+                <li style="color:gray;>
+                    <a href="/login">Login</a>
+                </li>
+                <li style="color:gray;>
+                    <a href="/register">Register</a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+
+            <h5 style="color:gray;">Pages</h5>
+            <ul ">
+                <li style="color:gray;>
+                    <a href="/">home</a>
+                </li >
+                <li style="color:gray;>
+                    <a href="/blog">Blog</a>
+                </li>
+                <li style="color:gray;>
+                    <a href="/login">Login</a>
+                </li>
+                <li style="color:gray;>
+                    <a href="/register">Register</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
